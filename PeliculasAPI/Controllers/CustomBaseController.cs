75e5d68bd497c63593dd974e54e46e43dd172954
Bobbypip <roroal314@gmail.com>
2021-04-12 13:52:39 -0500

    No Conflict

--- PeliculasAPI/Controllers/CustomBaseController.cs
+++ PeliculasAPI/Controllers/CustomBaseController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PeliculasAPI.Controllers
 {
-    //real based
+    //real based no Conflict
     public class CustomBaseController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
