5626bc6ecbc6f3118a82b2d6fd3568d37094abfb
Bobbypip <roroal314@gmail.com>
2021-04-12 11:01:00 -0500

    Soy Revert

--- PeliculasAPI/Controllers/GenresController.cs
+++ PeliculasAPI/Controllers/GenresController.cs
@@ -14,6 +14,7 @@ namespace PeliculasAPI.Controllers
     [Route("api/genres")]
     public class GenresController : CustomBaseController
     {
+        //Soy Revert
         public GenresController(
             ApplicationDbContext context,
             IMapper mapper)
